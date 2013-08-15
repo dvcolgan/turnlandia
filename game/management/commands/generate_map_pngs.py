1c9fe1a2fda04e061a5cf4e626681910a9e938e3
David S Colgan <david@davidscolgan.com>
2013-08-15 19:18:10 -0400

    minimap

--- game/management/commands/generate_map_pngs.py
+++ game/management/commands/generate_map_pngs.py
@@ -21,67 +21,52 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         squares = Square.objects.order_by('row', 'col')
-        #first = squares[0]
-        #last = squares[squares.count()-1]
 
         width = (MAX_COL - MIN_COL) * GRID_SIZE
         height = (MAX_ROW - MIN_ROW) * GRID_SIZE
 
         im = Image.new('RGB', (width, height), 'black')
 
-        #color_dict = {}
-        #for color in COLORS:
-        #    color_dict[color[0]] = (color[1], color[2])
-
         #http://effbot.org/imagingbook/imagedraw.htm
         draw = ImageDraw.Draw(im)
 
-        #for square in squares:
-        #    if square.col < -20:
-        #        continue
-        #    if square.row < -20:
-        #        continue
-        #    if square.col > 20:
-        #        continue
-        #    if square.row > 20:
-        #        continue
-        #    print square.col, square.row
-        #    if square.owner != None:
-        #        fill_color = square.owner.color
-        #    else:
-        #        fill_color = '#ffffff'
+        for square in squares:
+            print square.col, square.row
+            if square.owner != None:
+                fill_color = square.owner.color
+            else:
+                fill_color = '#ffffff'
 
-        #    x1 = square.col*GRID_SIZE+width/2
-        #    y1 = square.row*GRID_SIZE+height/2
-        #    x2 = square.col*GRID_SIZE+GRID_SIZE+width/2
-        #    y2 = square.row*GRID_SIZE+GRID_SIZE+height/2
+            x1 = square.col*GRID_SIZE+width/2
+            y1 = square.row*GRID_SIZE+height/2
+            x2 = square.col*GRID_SIZE+GRID_SIZE+width/2
+            y2 = square.row*GRID_SIZE+GRID_SIZE+height/2
 
-        #    draw.rectangle(((x1, y1), (x2, y2)), fill=fill_color, outline='#cccccc')
+            draw.rectangle(((x1, y1), (x2, y2)), fill=fill_color)
 
-        #    for i, unit in enumerate(square.units.all()):
-        #        if i == 0:
-        #            ax1 = x1 + GRID_SIZE/4 - GRID_SIZE/8
-        #            ay1 = y1 + GRID_SIZE/4 - GRID_SIZE/8
-        #            ax2 = x1 + GRID_SIZE/4 + GRID_SIZE/8
-        #            ay2 = y1 + GRID_SIZE/4 + GRID_SIZE/8
-        #        if i == 1:
-        #            ax1 = x1 + 3*GRID_SIZE/4 - GRID_SIZE/8
-        #            ay1 = y1 + 3*GRID_SIZE/4 - GRID_SIZE/8
-        #            ax2 = x1 + GRID_SIZE/4 + GRID_SIZE/8
-        #            ay2 = y1 + GRID_SIZE/4 + GRID_SIZE/8
-        #        if i == 2:
-        #            ax1 = x1 + 3*GRID_SIZE/4 - GRID_SIZE/8
-        #            ay1 = y1 + 3*GRID_SIZE/4 - GRID_SIZE/8
-        #            ax2 = x1 + 3*GRID_SIZE/4 + GRID_SIZE/8
-        #            ay2 = y1 + 3*GRID_SIZE/4 + GRID_SIZE/8
-        #        if i == 3:
-        #            ax1 = x1 + GRID_SIZE/4 - GRID_SIZE/8
-        #            ay1 = y1 + GRID_SIZE/4 - GRID_SIZE/8
-        #            ax2 = x1 + 3*GRID_SIZE/4 + GRID_SIZE/8
-        #            ay2 = y1 + 3*GRID_SIZE/4 + GRID_SIZE/8
+            for i, unit in enumerate(square.units.all()):
+                if i == 0:
+                    ax1 = x1 + GRID_SIZE/4 - GRID_SIZE/8
+                    ay1 = y1 + GRID_SIZE/4 - GRID_SIZE/8
+                    ax2 = x1 + GRID_SIZE/4 + GRID_SIZE/8
+                    ay2 = y1 + GRID_SIZE/4 + GRID_SIZE/8
+                if i == 1:
+                    ax1 = x1 + 3*GRID_SIZE/4 - GRID_SIZE/8
+                    ay1 = y1 + 3*GRID_SIZE/4 - GRID_SIZE/8
+                    ax2 = x1 + GRID_SIZE/4 + GRID_SIZE/8
+                    ay2 = y1 + GRID_SIZE/4 + GRID_SIZE/8
+                if i == 2:
+                    ax1 = x1 + 3*GRID_SIZE/4 - GRID_SIZE/8
+                    ay1 = y1 + 3*GRID_SIZE/4 - GRID_SIZE/8
+                    ax2 = x1 + 3*GRID_SIZE/4 + GRID_SIZE/8
+                    ay2 = y1 + 3*GRID_SIZE/4 + GRID_SIZE/8
+                if i == 3:
+                    ax1 = x1 + GRID_SIZE/4 - GRID_SIZE/8
+                    ay1 = y1 + GRID_SIZE/4 - GRID_SIZE/8
+                    ax2 = x1 + 3*GRID_SIZE/4 + GRID_SIZE/8
+                    ay2 = y1 + 3*GRID_SIZE/4 + GRID_SIZE/8
 
-        #        draw.arc(((ax1, ay1, ax2, ay2)), 0, 360, fill=unit.owner.color)
+                draw.ellipse(((ax1, ay1, ax2, ay2)), outline='#000000', fill=unit.owner.color)
 
-        draw.arc(((1000, 1000, 1200, 1200)), 0, 360, outline='#ffff88')
         im.save('static/images/minimap.png', 'PNG')
         print 'Saved full image'
